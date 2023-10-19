5b2539fb8f4df91e3956b1e9b9afb43db27aaf4f
bebeaubn <yebin0213@naver.com>
2023-10-19 17:07:52 +0900

    초기 업로드

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.babakaud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
